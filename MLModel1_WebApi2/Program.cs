f5f540141c1657651a526bbac1c2701b7f7cdcd8
Tim Heuer <tim@timheuer.com>
2021-08-26 17:08:35 -0700

    Add authorization services

--- MLModel1_WebApi2/Program.cs
+++ MLModel1_WebApi2/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPredictionEnginePool<MLModel1.ModelInput, MLModel1.ModelOutput>().FromFile("MLModel1.zip");
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthorization();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "MLModel1", Version = "v1" });
